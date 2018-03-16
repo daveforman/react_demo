114bad99f504d281e593c2af733ad884025b4455
Dave <dave@stackden.com>
2018-03-16 16:34:25 -0700

    Intro demo complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+
+const user = {
+    first: "Dave",
+    last: "Forman"
+}
+
+function greetUser(user){
+    return user.first + ' ' + user.last;
+}
+
+const greeting = <h1>Hello {greetUser(user)}</h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
